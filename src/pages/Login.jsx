c92794db6aec5e3689e76916ad0fb2532f18d729
Carlos Eduardo <carlao0010@hotmail.com>
2023-04-14 19:35:44 -0300

    Requisito 2

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,13 +1,97 @@
 import React from 'react';
+import PropTypes from 'prop-types';
+import { createUser } from '../services/userAPI';
+import Loading from './Loading';
 
 class Login extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      name: '',
+      buttonDisable: true,
+      loading: false,
+    };
+
+    this.handleChange = this.handleChange.bind(this);
+    this.handleButtonClick = this.handleButtonClick.bind(this);
+  }
+
+  handleChange({ target }) {
+    const { name, value } = target;
+
+    if (value.length > 2) {
+      this.setState({
+        [name]: value,
+        buttonDisable: false,
+      });
+    } else {
+      this.setState({
+        [name]: value,
+        buttonDisable: true,
+      });
+    }
+  }
+
+  handleButtonClick(event) {
+    event.preventDefault();
+
+    this.setState({
+      loading: true,
+    });
+
+    const { name } = this.state;
+    const { history: { push } } = this.props;
+
+    createUser({ name }).then(() => {
+      push('/search');
+    });
+  }
+
   render() {
+    const { name, buttonDisable, loading } = this.state;
+
     return (
       <div data-testid="page-login">
-        <h1>Login</h1>
+        {loading
+          ? <Loading />
+          : (
+            <form>
+              <fieldset>
+                <legend>Login</legend>
+                <label htmlFor="name">
+                  Name
+                  {' '}
+                  <input
+                    type="text"
+                    id="name"
+                    name="name"
+                    placeholder="Enter your name"
+                    data-testid="login-name-input"
+                    onChange={ this.handleChange }
+                    value={ name }
+                  />
+                </label>
+                <button
+                  id="btnLogin"
+                  data-testid="login-submit-button"
+                  disabled={ buttonDisable }
+                  onClick={ this.handleButtonClick }
+                >
+                  Entrar
+                </button>
+              </fieldset>
+            </form>
+          )}
       </div>
     );
   }
 }
+
+Login.propTypes = {
+  history: PropTypes.shape({
+    push: PropTypes.func.isRequired,
+  }).isRequired,
+};
 
 export default Login;
