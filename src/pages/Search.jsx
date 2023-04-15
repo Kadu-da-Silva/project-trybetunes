698b7618e4215afad0b6c32ca4f3f39903bdb520
Carlos Eduardo <carlao0010@hotmail.com>
2023-04-14 22:56:33 -0300

    Requisito 6

--- src/pages/Search.jsx
+++ src/pages/Search.jsx
@@ -1,5 +1,8 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import Header from '../Components/Header';
+import searchAlbumsAPI from '../services/searchAlbumsAPI';
+import Loading from './Loading';
 
 class Search extends React.Component {
   constructor(props) {
@@ -7,11 +10,16 @@ class Search extends React.Component {
 
     this.state = {
       name: '',
+      nameArtist: '',
       buttonDisable: true,
-      // loading: false,
+      loading: false,
+      resultSearch: false,
+      albums: [],
     };
 
     this.handleChange = this.handleChange.bind(this);
+    this.handleButtonClick = this.handleButtonClick.bind(this);
+    this.resultSearch = this.resultSearch.bind(this);
   }
 
   handleChange({ target }) {
@@ -30,8 +38,57 @@ class Search extends React.Component {
     }
   }
 
+  handleButtonClick(event) {
+    event.preventDefault();
+
+    const { name } = this.state;
+
+    searchAlbumsAPI(name)
+      .then((date) => {
+        this.setState({
+          loading: true,
+          resultSearch: true,
+          nameArtist: name,
+          albums: date,
+          name: '',
+        });
+      });
+  }
+
+  resultSearch() {
+    const { albums, nameArtist } = this.state;
+
+    if (albums.length !== 0) {
+      return (
+        <>
+          <h1>{`Resultado de álbuns de: ${nameArtist}`}</h1>
+          {albums.map((obj) => (
+            <Link
+              key={ obj.collectionId }
+              data-testid={ `link-to-album-${obj.collectionId}` }
+              to={ `/album/${obj.collectionId}` }
+            >
+              <div>
+                <img src={ obj.artworkUrl100 } alt="" />
+                <p>{`${obj.artistName}`}</p>
+                <p>{`${obj.collectionName}`}</p>
+                <p>{`${obj.collectionPrice}`}</p>
+                <p>{`${obj.releaseDate}`}</p>
+                <p>{`${obj.trackCount} músicas` }</p>
+              </div>
+            </Link>
+          ))}
+        </>
+      );
+    }
+    return <p>Nenhum álbum foi encontrado</p>;
+  }
+
   render() {
-    const { name, buttonDisable } = this.state;
+    const { name, buttonDisable, loading, resultSearch } = this.state;
+    if (loading === true) {
+      <Loading />;
+    }
     return (
       <div data-testid="page-search">
         <Header />
@@ -52,11 +109,15 @@ class Search extends React.Component {
               id="btnSearch"
               data-testid="search-artist-button"
               disabled={ buttonDisable }
+              onClick={ this.handleButtonClick }
             >
               Pesquisar
             </button>
           </fieldset>
         </form>
+
+        {resultSearch ? this.resultSearch() : undefined}
+
       </div>
     );
   }
